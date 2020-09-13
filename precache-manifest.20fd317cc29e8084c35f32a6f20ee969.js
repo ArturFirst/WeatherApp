61fa8e1863438718c2d28084c594b7b471d51bf3
ArturFirst <z.view007@gmail.com>
2020-09-13 12:06:11 +0300

    Updates

---
+++ precache-manifest.20fd317cc29e8084c35f32a6f20ee969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291e40c96d64a007c2d1f2683bd8319f",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "63ca87d71e509fd436a4",
+    "url": "/WeatherApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a345d76d02dd79e2060b",
+    "url": "/WeatherApp/static/js/2.c22e8293.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/WeatherApp/static/js/2.c22e8293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ca87d71e509fd436a4",
+    "url": "/WeatherApp/static/js/main.3bba2b56.chunk.js"
+  },
+  {
+    "revision": "f34a18f0592fbbc8ce7e",
+    "url": "/WeatherApp/static/js/runtime-main.dcd8b410.js"
+  }
+]);
